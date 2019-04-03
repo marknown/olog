e1793dbad25c713f7d483f1d1afa5d0db09e76c0
marknown <tailcome@qq.com>
2019-04-03 11:37:14 +0800

    olog v1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # olog
 olog is a go log package
+
+## SetDebug
+```
+olog.SetDebug(true)
+olog.SetDebug(false)
+```
+
+## Printf
+```
+olog.Printf() usage like log.Printf()
+```
+
+## Println
+```
+olog.Printf() usage like log.Println()
+```
---
+++ olog.go
@@ -0,0 +1,39 @@
+// Package olog is own log
+package olog
+
+import (
+	"log"
+)
+
+// log.SetPrefix("===>")
+// log.SetFlags(log.Ldate | log.Ltime | log.Lmicroseconds)
+// log.SetFlags(log.Ldate | log.Ltime | log.Lmicroseconds | log.Lshortfile)
+
+var packageIsDebug = true
+
+func init() {
+	// log.SetPrefix("===>")
+	log.SetFlags(log.Ldate | log.Ltime | log.Lmicroseconds | log.Lshortfile)
+	// log.SetFlags(log.Ldate | log.Ltime | log.Lmicroseconds | log.Lshortfile)
+}
+
+// SetDebug 设置是否开启调试
+func SetDebug(isDebug bool) {
+	packageIsDebug = isDebug
+}
+
+// Printf 格式化输出日志
+func Printf(format string, v ...interface{}) {
+	if packageIsDebug {
+		log.SetFlags(log.Ldate | log.Ltime | log.Lmicroseconds)
+		log.Printf(format, v...)
+	}
+}
+
+// Println 输出并换行
+func Println(v ...interface{}) {
+	if packageIsDebug {
+		log.SetFlags(log.Ldate | log.Ltime | log.Lmicroseconds)
+		log.Println(v...)
+	}
+}
